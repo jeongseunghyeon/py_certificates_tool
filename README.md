942decbdba64f878531b84f86dba40b5f31d79bd
jsh152169 <jsh152169@netmarble.com>
2022-01-07 18:53:49 +0900

    csr create modal 개발중

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# py_certificate_tool
+### Python Base로 작성된 보안인증서 유틸 툴 입니다.
+## PyQT5를 사용한 windows application 입니다.
+
+## 현재 개발중입니다.(2022 상반기내에 1.0 version release 예정)
+## 제공기능(예정) : CSR 생성, 인증서 포맷 변경, 인증서 암호 설정/해제
+
+1. Init
+   - pip install -r requirements.txt
+   - python app.py
+
+2. Build
+   - pyinstall -w (--onefile) app.py
--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QMainWindow, QAction, qApp, QDesktopWidget
-from PyQt5.QtWidgets import QLabel, QGridLayout, QLineEdit, QTextEdit, QFileDialog, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication, QDateTime, QTimer
 
 
-class CertificateToolView(QMainWindow):
+class MainView(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -17,6 +16,9 @@ class CertificateToolView(QMainWindow):
         self.chain2_path = QLineEdit()
         self.rootca_path = QLineEdit()
         self.cert_contents = QTextEdit()
+
+        # Variable
+        self._csr_temp = ''
 
         self.datetime = QDateTime.currentDateTime()
         self.datetime_label = ''
@@ -64,6 +66,7 @@ class CertificateToolView(QMainWindow):
         grid = QGridLayout()
         cw.setLayout(grid)
 
+        self.create_csr_btn.clicked.connect(self.onclick_create_csr_btn)
         grid.addWidget(self.create_csr_btn, 0, 0, 1, 3)
 
         grid.addWidget(QLabel('crt file : '), 1, 0)
@@ -113,6 +116,19 @@ class CertificateToolView(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def onclick_create_csr_btn(self):
+        win = CreateCSRView()
+        res = win.show_modal()
+        if res:
+            country_name = win.country_name.text()
+            state_name = win.state_name.text()
+            locality_name = win.locality_name.text()
+            org_name = win.org_name.text()
+            org_unit_name = win.org_unit_name.text()
+            common_name = win.common_name.text()
+            email = win.email.text()
+            print(f'{country_name} {state_name} {locality_name} {org_name} {org_unit_name} {common_name} {email}')
+
     def onclick_crt_file_open_btn(self):
         file_name = QFileDialog.getOpenFileName(self)
         if file_name[0]:
@@ -132,8 +148,69 @@ class CertificateToolView(QMainWindow):
                 self.cert_contents.setText(data)
 
 
+class CreateCSRView(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Init Component
+        self.country_name = QLineEdit()
+        self.state_name = QLineEdit()
+        self.locality_name = QLineEdit()
+        self.org_name = QLineEdit()
+        self.org_unit_name = QLineEdit()
+        self.common_name = QLineEdit()
+        self.email = QLineEdit()
+
+        self.init_ui()
+        self.move_to_center()
+
+    def init_ui(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        grid.addWidget(QLabel('Country name(2 letters) : '), 1, 0, 1, 2)
+        grid.addWidget(self.country_name, 1, 2, 1, 4)
+        grid.addWidget(QLabel('State or Province Name : '), 2, 0, 1, 2)
+        grid.addWidget(self.state_name, 2, 2, 1, 4)
+        grid.addWidget(QLabel('Locality Name : '), 3, 0, 1, 2)
+        grid.addWidget(self.locality_name, 3, 2, 1, 4)
+        grid.addWidget(QLabel('Organization Name : '), 4, 0, 1, 2)
+        grid.addWidget(self.org_name, 4, 2, 1, 4)
+        grid.addWidget(QLabel('Organization Unit Name : '), 5, 0, 1, 2)
+        grid.addWidget(self.org_unit_name, 5, 2, 1, 4)
+        grid.addWidget(QLabel('Common Name(Domain) : '), 6, 0, 1, 2)
+        grid.addWidget(self.common_name, 6, 2, 1, 4)
+        grid.addWidget(QLabel('Email Address : '), 7, 0, 1, 2)
+        grid.addWidget(self.email, 7, 2, 1, 4)
+
+        create_btn = QPushButton('Create')
+        create_btn.clicked.connect(self.onclick_create_btn)
+        grid.addWidget(create_btn, 8, 0, 1, 3)
+        cancel_btn = QPushButton('Cancel')
+        cancel_btn.clicked.connect(self.onclick_cancel_btn)
+        grid.addWidget(cancel_btn, 8, 3, 1, 3)
+
+        self.setWindowTitle('Create CSR(Certificate Signing Request) File')
+        self.resize(600, 400)
+
+    def onclick_create_btn(self):
+        self.accept()
+
+    def onclick_cancel_btn(self):
+        self.reject()
+
+    def move_to_center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def show_modal(self):
+        return super().exec_()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = CertificateToolView()
+    ex = MainView()
     sys.exit(app.exec_())
 
